cd6b73f5a8a2fdf9bac1fab599d310c2569cf380
Koushik <koushik0822@gmail.com>
2025-08-10 17:07:07 +0000

    all thse files from 45sxz t this same it should be

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   "projectId": "battlecode-rwb92",
   "appId": "1:434917292342:web:30b8809cc3a2c1a3d09b97",
-  "storageBucket": "battlecode-rwb92.firebasestorage.app",
+  "storageBucket": "battlecode-rwb92.appspot.com",
   "apiKey": "AIzaSyCvCpLguhA7u-vOu-dSX2MR8frznFPQ5vY",
   "authDomain": "battlecode-rwb92.firebaseapp.com",
   "measurementId": "",
@@ -19,5 +20,6 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { app, db, auth };
+export { app, db, auth, storage };
